9b526a264fbc948f91cf742e52177828c50cdbf7
deadmouze <mateusz.kierman@gmail.com>
2022-06-14 19:29:21 +0200

    update

--- zadania/src/Zad2.java
+++ zadania/src/Zad2.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Zad2 {
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+public class Zad2 {
+    public static void main(String[] args) { //1.
+
+        try {
+            URL url = new URL("https://www.youtube.com");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            String currentline;
+            boolean slowo = false;
+//Napisz program, który ze strony internetowej o wskazanym adresie wyświetla tylko te linie, które zawierają znak “@”. [Operacje wejścia-wyjścia + operacja na tekście].
+            while ((currentline = br.readLine()) != null) {
+
+                if (currentline.contains("@")) {
+                    slowo = true;
+                    System.out.println(currentline);
+                }
+            }
+            if (!slowo) {
+                System.out.println("Nie znaleziono danego slowa");
+            }
+
+
+            //Napisz program, który ze strony internetowej o wskazanym adresie, wyświetla tylko te linie, które zawierają znak “@”, pod warunkiem że  nie występują w @media [Operacje wejścia-wyjścia + operacja na tekście].
+            while ((currentline = br.readLine()) != null) {
+
+                if (currentline.contains("@media")) {
+                    continue;
+                } else if (currentline.contains("@")) {
+                    slowo = true;
+                    System.out.println("\n\n" + currentline);
+                }
+            }
+            if (!slowo) {
+                System.out.println("Nie znaleziono danego slowa");
+            }
+        } catch (IOException ex) {
+            System.out.println("Błąd");
+        }
+    }
 }
--- zadania/src/Zad3.java
+++ zadania/src/Zad3.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Zad3 {
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+public class Zad3 {
+    public static void main(String[] args) { //1.
+
+        String currentline;
+        boolean adres = false;
+
+        try {
+            URL url = new URL("https://www.facebook.com");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            while ((currentline = br.readLine()) != null) {
+                String[] slowa = currentline.split(",");
+                for (String slowo : slowa) {
+                    if (slowo.matches(" /[a-zA-Z\\d.! #$%&'*+/=?^_`{|}~-]+@[a-zA-Z\\d-]+(?:\\. [a-zA-Z\\d-]+)*/")) {
+                        adres = true;
+                        System.out.println(slowo);
+                    }
+                }
+            }
+            if (!adres) {
+                System.out.println("Nie znaleziono danego slowa");
+            }
+
+
+        } catch (IOException ex) {
+            System.out.println("Błąd");
+        }
+    }
 }
